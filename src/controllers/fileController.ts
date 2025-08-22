fcb95f454661b6128c4c5de1bad145852fc10eeb
mubashir <mubashir.ali@dror.co.in>
2025-08-22 16:38:26 +0530

    changes

--- src/controllers/fileController.ts
+++ src/controllers/fileController.ts
@@ -30,68 +30,31 @@ export const createFolder = (req: Request, res: Response) => {
   }
 };
 
-// // Upload file into an agent’s folder
-// export const uploadFile = (req: Request, res: Response) => {
-//   try {
-//     const { agentId, folderName } = req.body;
-
-//     if (!req.file) {
-//       return res.status(400).json({ error: "No file uploaded" });
-//     }
-
-//     const folderPath = path.join(UPLOADS_DIR, agentId, folderName || "");
-//     if (!fs.existsSync(folderPath)) {
-//       fs.mkdirSync(folderPath, { recursive: true });
-//     }
-
-//     const filePath = path.join(folderPath, req.file.originalname);
-//     fs.renameSync(req.file.path, filePath);
-//     const fileUrl = `${req.protocol}://${req.get(
-//       "host"
-//     )}/uploads/${agentId}/${folderName}/${req.file.filename}`;
-
-//     res.status(201).json({
-//       message: "File uploaded successfully",
-//       file: {
-//         name: req.file.originalname,
-//         path: filePath,
-//         size: req.file.size,
-//         mimetype: req.file.mimetype,
-//         fileUrl,
-//       },
-//     });
-//   } catch (error: any) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
 // Upload file into an agent’s folder
-
 export const uploadFile = (req: Request, res: Response) => {
-  const UPLOADS_DIR = path.join(__dirname, "../../uploads");
   try {
-    const { agentId, folderName = "" } = req.body;
+    const { agentId, folderName } = req.body;
 
     if (!req.file) {
       return res.status(400).json({ error: "No file uploaded" });
     }
 
-    const folderPath = path.join(UPLOADS_DIR, agentId, folderName);
+    const folderPath = path.join(UPLOADS_DIR, agentId, folderName || "");
     if (!fs.existsSync(folderPath)) {
       fs.mkdirSync(folderPath, { recursive: true });
     }
 
-    const finalFileName = req.file.originalname;
-    const finalFilePath = path.join(folderPath, finalFileName);
-    fs.renameSync(req.file.path, finalFilePath);
-
+    const filePath = path.join(folderPath, req.file.originalname);
+    fs.renameSync(req.file.path, filePath);
     const fileUrl = `${req.protocol}://${req.get(
       "host"
-    )}/uploads/${agentId}/${folderName}/${req.file.originalname}`;
+    )}/uploads/${agentId}/${folderName}/${req.file.filename}`;
+
     res.status(201).json({
       message: "File uploaded successfully",
       file: {
-        name: finalFileName,
-        path: finalFilePath,
+        name: req.file.originalname,
+        path: filePath,
         size: req.file.size,
         mimetype: req.file.mimetype,
         fileUrl,
